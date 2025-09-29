54b0905add7f0428bbdca3e1520d48b84263955e
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2025-09-29 18:01:19 -0300

    Depending on what MapAction overload is called: Sync/Async: - vitraux.js: Make registerAction Sync/Async - Make the registerAction calling generator (IRegisterActionCall) Sync/Async

--- src/JsCodeGeneration/BuiltInCalling/Actions/RegisterActionCall.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Vitraux.JsCodeGeneration.BuiltInCalling.Actions;
-internal class RegisterActionCall : IRegisterActionCall
-{
-    public string Generate(string elementsArg, string eventArg, string vmKeyArg, string actionKeyArg, string actionArgsCallbackArg)
-        => $"globalThis.vitraux.actions.registerAction({elementsArg},{eventArg}),{vmKeyArg}, {actionKeyArg}, {actionArgsCallbackArg})";
-}
-
---
+++ src/JsCodeGeneration/BuiltInCalling/Actions/RegisterActionSyncCall.cs
@@ -0,0 +1,8 @@
+﻿namespace Vitraux.JsCodeGeneration.BuiltInCalling.Actions;
+
+internal class RegisterActionSyncCall : IRegisterActionSyncCall
+{
+    public string Generate(string elementsArg, string eventArg, string vmKeyArg, string actionKeyArg, string actionArgsCallbackArg)
+        => $"globalThis.vitraux.actions.registerActionSync({elementsArg},{eventArg}),{vmKeyArg}, {actionKeyArg}, {actionArgsCallbackArg})";
+}
+
