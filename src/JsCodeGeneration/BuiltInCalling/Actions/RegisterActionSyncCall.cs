3ea7d8f57b46f6bab440f2cf5786d4a8d56f870a
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2025-10-01 13:01:13 -0300

    MapAction DSL refactor: - Change the DSL modeling interfaces (just the interfaces, it doesn't build) to allow users to add many FromInputs, but just one set of parameters per MapAction. Now, a MapAction() may have just a set of parameters. - On() methods, for action mappings, now receive an array params of string. - Modify PetOwnerConfiguration to reflect a combination of cases for MapAction(). - Fix RegisterActionCall implementations: Add quotes to params of JS code method calling

--- src/JsCodeGeneration/BuiltInCalling/Actions/RegisterActionSyncCall.cs
+++ src/JsCodeGeneration/BuiltInCalling/Actions/RegisterActionSyncCall.cs
@@ -3,6 +3,6 @@
 internal class RegisterActionSyncCall : IRegisterActionSyncCall
 {
     public string Generate(string elementsArg, string eventArg, string vmKeyArg, string actionKeyArg, string actionArgsCallbackArg)
-        => $"globalThis.vitraux.actions.registerActionSync({elementsArg},{eventArg}),{vmKeyArg}, {actionKeyArg}, {actionArgsCallbackArg})";
+        => $"globalThis.vitraux.actions.registerActionSync({elementsArg},'{eventArg}','{vmKeyArg}', '{actionKeyArg}', {actionArgsCallbackArg})";
 }
 
--- src/Modeling/Building/Contracts/ElementBuilders/Actions/IRootParametrizableActionSourceFinallizableBuilder.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Vitraux.Modeling.Building.Contracts.ElementBuilders.Actions;
-
-public interface IRootParametrizableActionSourceFinallizableBuilder<TViewModel> : IRootParametrizableActionSourceBuilder<TViewModel>, IModelMapper<TViewModel>
-{
-}
