a36a6a11b72302179e83ce4f5deb1ca9bb5ec18f
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2025-10-03 17:51:46 -0300

    - Create IActionKeyGenerator interface and implemention - Generate the action key in ModelMapper.MapAction to create ActionData objects - Remove action key generation from RootActionEventRegistrationJsGenerator (it still needs the refactor because of DSL changes)

---
+++ src/Modeling/Building/Implementations/ElementBuilders/Actions/ActionKeyGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Vitraux.Helpers;
+using Vitraux.Modeling.Building.Contracts.ElementBuilders.Actions;
+
+namespace Vitraux.Modeling.Building.Implementations.ElementBuilders.Actions;
+
+internal class ActionKeyGenerator(IAtomicAutoNumberGenerator atomicAutoNumberGenerator) : IActionKeyGenerator
+{
+    public string Generate()
+        => $"ak{atomicAutoNumberGenerator.Next()}";
+}
